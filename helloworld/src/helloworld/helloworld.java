7be2308b64110c71294a8fd833b31283e76cd495
Kunal <kunalnyadav300@gmail.com>
2022-12-05 08:20:20 +0530

    Project 200 Day 10

--- helloworld/src/helloworld/helloworld.java
+++ helloworld/src/helloworld/helloworld.java
@@ -1,49 +1,15 @@
 package helloworld;
 
-import java.util.Arrays;
-
 public class helloworld {
 
-	
-	// Driver Code
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-//		int[] a= new int[4];
-//		
-//		for(int elem:a)
-//		{
-//			System.out.print(elem);
-//		}
-//		System.out.println();
-//		Arrays.fill(a,5);
-//		for(int elem:a)
-//		{
-//			System.out.print(elem);
-//		}
-//		System.out.println();
-//		
-//		int[] ar= {1,2,0,0,0,4,5};
-//		Arrays.fill(ar, 2, 5, 10);
-//		for(int elem:ar)
-//		{
-//			System.out.print(elem + " ");
-//		}
-//		System.out.println();
-		int x=123;
-		System.out.println(x%10);
-		System.out.println(x/10);
-		
-//		int[] arr= {20,30,50,10,25,60};
-//		Arrays.sort(arr);//Sort the element
-//		for(int elem:arr)
-//		{
-//			System.out.print(elem + " ");
-//		}
-//		System.out.println();
-//		int res=Arrays.binarySearch(arr, 30);//inbuilt bs method with parameter
-//		System.out.println(res);
-//		
-//		
+	int n=5;
+		    int a = 1, b = 1;
+		    while (n-- > 0) {
+		        a = (b += a) - a;
+		    System.out.println("a "+ a+" b "+b);}
+		  System.out.println(a);
+			
 	}
 }
